ed35045d312c1f8ffc60a81839a2d320f597bba1
nuru <nurudiin222@gmail.com>
2023-04-28 14:41:51 +0300

    Added Like button/Wishlists

--- cake.js
+++ cake.js
@@ -1,1 +1,21 @@
 //  Code Here!
+
+let likeBtns;
+  
+document.addEventListener('DOMContentLoaded', () => {
+  likeBtns = document.querySelectorAll('.like');
+  likeBtns.forEach(likeBtn => {
+    likeBtn.addEventListener('click', toggleLike);
+  });
+});
+
+function toggleLike(event) {
+  const likeBtn = event.currentTarget;
+  const likeImg = likeBtn.querySelector('img');
+  
+  if (likeImg.src.includes('Like.png')) {
+    likeImg.src = './img/unlike.png';
+  } else {
+    likeImg.src = './img/Like.png';
+  }
+}
